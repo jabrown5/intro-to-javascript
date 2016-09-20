1e0e91d959e8eb4ebaf894abcf5f9a6f670f9da4
Jennifer Brown <j.a.brown.5@gmail.com>
2016-09-20 11:30:10 -0500

    window.onload added

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+window.onload = function(evt) {
+    // window object's ability to do things onload (upon loading)
+    console.log("The page has loaded!");
+    console.log("Thanks, window.onload");
+};
